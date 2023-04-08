a8d68bcd11a5596694ab72313a4b3dd7a2d7d423
devansh dwivedi <devanshdwivedi68@gmail.com>
2023-04-08 18:31:36 +0530

    init

--- client/src/components/Header/mainHeader.js
+++ client/src/components/Header/mainHeader.js
@@ -4,10 +4,7 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
+        
       </nav>
     </header>
   );
