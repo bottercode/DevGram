3c94d10ad8e0c46dbb905bbbe69bd049ecad26ab
devansh dwivedi <devanshdwivedi68@gmail.com>
2023-04-16 23:58:14 +0530

    changes made in message

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import classes from  "./message.module.css"
+import GroupsIcon from '@mui/icons-material/Groups';
+import StreamIcon from '@mui/icons-material/Stream';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhoneIcon from '@mui/icons-material/Phone';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import SendIcon from '@mui/icons-material/Send';
 
 const Message = (props) => {
   return (
@@ -10,8 +17,8 @@ const Message = (props) => {
         </div>
         <h2 className={classes.h2}>Messages </h2>
           <input className={classes.searchbar} type ="text" placeholder="Search messages"/>
-         
-          <button className={classes.button}>Create Group</button>
+            <GroupsIcon />
+            <button className={classes.button}>Create Group</button>
           <div className={classes.friend_profile1}>
             <img src={props.src} alt="User Profile" />
             <h2 className={classes.h2}>{props.name}</h2>
@@ -25,6 +32,7 @@ const Message = (props) => {
             <h2 className={classes.h2}>{props.name}</h2>
           </div>
           <button className={classes.live_button}>
+            <StreamIcon />
             <h3 className={classes.live}>Live</h3>
           <h3 className={classes.h3}>Join as Saurabh </h3>
 
@@ -32,6 +40,15 @@ const Message = (props) => {
       </div>
       <div className={classes.right}>
         <h1 className={classes.brandName}>Pro-Gram</h1>
+        <VideocamIcon />
+        <PhoneIcon />
+        
+        <div className={classes.type_text} type ="text" placeholder="Search messages">
+          <KeyboardVoiceIcon />
+          <AttachFileIcon />
+          <SendIcon />
+        </div>
+       
       </div>
     </div>
   )
