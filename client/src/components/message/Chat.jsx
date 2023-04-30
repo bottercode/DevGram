d64b73ff4185e2144a47a8c968379e8ceef62c00
devansh dwivedi <devanshdwivedi68@gmail.com>
2023-04-30 23:23:46 +0530

    updated chat component

--- client/src/components/message/Chat.jsx
+++ client/src/components/message/Chat.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import classes from './chat.module.css'
-import { useHistory } from 'react-router-dom'
+import React from 'react';
+import classes from './chat.module.css';
+import { useHistory } from 'react-router-dom';
 import { io } from "socket.io-client";
 import { useRef } from 'react';
-import { host } from "../../APIRoutes"
+import { host } from "../../APIRoutes";
 import { useState } from 'react';
 import { useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import Welcome from '../welcome/Welcome';
 import ChatContainer from '../chatContainer/ChatContainer';
 
-
 const Chat = () => {
   const history = useHistory();
   const socket = useRef();
-  const [contacts, setContacts] = useState([])
-  const [currentChat, setCurrentChat] = useState(undefined)
-  const [currentuser, setcurrentuser] = useState(undefined)
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentuser, setcurrentuser] = useState(undefined);
 
   useEffect(() => {
-    const func = async() => {
-      if(!localStorage.getItem("Pro-Gram")){
-        history.push("/login")
-      }
-      else {
-        const data =  await JSON.parse(localStorage.getItem("Pro-Gram")).username
-        setcurrentuser(data)
+    const func = async () => {
+      if (!localStorage.getItem("Pro-Gram")) {
+        history.push("/login");
+      } else {
+        const data = await JSON.parse(localStorage.getItem("Pro-Gram")).username;
+        setcurrentuser(data);
       }
     }
     func();
-  }, [history])
+  }, [history]);
 
   useEffect(() => {
     if (currentuser) {
@@ -38,24 +36,22 @@ const Chat = () => {
     }
   }, [currentuser]);
 
-  
-  const handleChatChange = (chat) =>{
+  const handleChatChange = (chat) => {
     setCurrentChat(chat);
-  } 
+  };
 
   return (
     <div className={classes.chat}>
       <div className={classes.ChatContainer}>
-        <Contacts contacts={contacts} changeChat={handleChatChange}/>
+        <Contacts contacts={contacts} changeChat={handleChatChange} />
         {currentuser === undefined ? (
           <Welcome />
-        ):(
+        ) : (
           <ChatContainer username={currentuser} currentChat={currentChat} socket={socket.current} />
-
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
